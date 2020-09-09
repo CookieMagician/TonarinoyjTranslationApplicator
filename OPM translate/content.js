ba95c8eeddc02c170edfe7278d7f7e0e62974ee3
Kristoffer Zaar <dv19kzr@cs.umu.se>
2020-09-09 11:13:37 +0200

    Redesigned the interface of the popup page.
    Some redone functionality.

--- OPM translate/content.js
+++ OPM translate/content.js
@@ -2,10 +2,10 @@ var element = document.getElementsByClassName("image-container")[0];
 
 let positions = element.getElementsByTagName('p');
 
+
 var length = positions.length;
 
 var chapterCode = location.href.replace("https://tonarinoyj.jp/episode/", "");;
-
 
 
 chrome.storage.sync.get(['currentHost'], function(result) {
@@ -13,9 +13,11 @@ chrome.storage.sync.get(['currentHost'], function(result) {
   var host = result.currentHost;
 
   for (var i = 0; i < length; i++) {
+    positions[i].style = "position: relative;";
+
     var newImg = new Image();
     newImg.src = host + chapterCode + "/"+i+".png";
-    newImg.style = "position: absolute;"
+    newImg.style = "position: absolute; z-index: 1;";
     newImg.className = "page-image js-page-image";
     positions[i].appendChild(newImg);
   }
--- OPM translate/popup.js
+++ OPM translate/popup.js
@@ -5,62 +5,63 @@ var options = document.getElementById('options');
 var hostTag = document.getElementById('host');
 var translationApplied = false;
 
-document.addEventListener("click", testURL);
+
+
+
+document.addEventListener("DOMContentLoaded", checkURL);
+button.addEventListener("click", inject);
 options.addEventListener("click", openOptions);
 
-function testURL() {
+var chapterCode;
 
+function checkURL() {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
 
-  if (translationApplied == true) {
-    greet.innerHTML = "Translations should be applied";
-    button.style = "display:none";
-  } else {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
 
-      let url = tabs[0].url;
+    if (url.search("https://tonarinoyj.jp/episode/") == -1) {
+      greet.innerHTML = "Not on tonarinoyj.jp";
+    } else {
+      chapterCode = url.replace("https://tonarinoyj.jp/episode/", "");
 
+      chrome.storage.sync.get(['currentHost'], function(result) {
 
+      var host = result.currentHost;
 
-      if (url.search("https://tonarinoyj.jp/episode/") == -1) {
-        greet.innerHTML = "Not on tonarinoyj.jp";
+      if (host === undefined) {
+          greet.innerHTML = "You have not applied a host for images. Please do so in settings";
+          options.style = "display: inline;"
+
       } else {
-        greet.innerHTML = "CHECKING...";
-        var chapterCode = url.replace("https://tonarinoyj.jp/episode/", "");
+          $.ajax({
+            url:host + chapterCode + "/0.png",
+            type:'HEAD',
+            error: function()
+            {
+              greet.innerHTML = "No translation for this chapter!";
+            },
+            success: function()
+            {
+              greet.innerHTML = "Chapter translation found, click to apply!";
+              button.style = "display: inline;"
 
-
-        chrome.storage.sync.get(['currentHost'], function(result) {
-
-          var host = result.currentHost;
-
-
-          if (host === undefined) {
-            greet.innerHTML = "You have not applied a host for images. Please do so in settings";
-            options.style = "display: inline;"
-          } else {
-
-            $.ajax({
-              url:host + chapterCode + "/0.png",
-              type:'HEAD',
-              error: function()
-              {
-                greet.innerHTML = "There is no translation for the chapter yet (or there is a server error)";
-              },
-              success: function()
-              {
-                greet.innerHTML = "The chapter translation is being applied";
-                translationApplied = true;
-                button.style = "display: none;";
-                chrome.tabs.executeScript({
-                  file: 'content.js'
-                });
-              }
-            });
-          }
-        });
-      }
-    });
-  }
+            }
+          });
+        }
+      });
+    }
+  });
 }
+
+function inject() {
+  greet.innerHTML = "The chapter translation is applied!";
+  translationApplied = true;
+  button.style = "display: none;";
+  chrome.tabs.executeScript({
+      file: 'content.js'
+  });
+}
+
 
 function openOptions() {
   chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
